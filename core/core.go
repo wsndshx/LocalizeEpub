81ea92d0d4031a2126080167bbc567053dcab277
zzlwd <i@fishfish.date>
2021-07-27 21:59:19 +0800

    epub文件的打开, 解压

---
+++ core/core.go
@@ -0,0 +1,77 @@
+package core
+
+import (
+	"archive/zip"
+	"errors"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+
+	"github.com/psanford/memfs"
+)
+
+var rootFS *memfs.FS
+var target string = ""
+
+func init() {
+	//新建一个文件系统
+	rootFS = memfs.New()
+}
+
+// Open 打开输入的epub文件
+func Open(Path string) error {
+	// 判断文件路径合法性
+	_, err := os.Lstat(Path)
+	if err != nil {
+		return err
+	}
+	// 判断是否为epub文件
+	if path.Ext(Path) != ".epub" {
+		return errors.New("所选的文件不是epub文件")
+	}
+	// 返回状态
+	return nil
+}
+
+func Unzip(path string) error {
+	// 解压目标文件
+	miao, err := zip.OpenReader(path)
+	defer miao.Close()
+	if err != nil {
+		return err
+	}
+
+	for _, file := range miao.File {
+		unzippath := filepath.Join(target, file.Name)
+		if file.FileInfo().IsDir() {
+			err := rootFS.MkdirAll(unzippath, file.Mode())
+			if err != nil {
+				return err
+			}
+			continue
+		}
+
+		fileReader, err := file.Open()
+		if err != nil {
+			return err
+		}
+		defer fileReader.Close()
+
+		data, _ := ioutil.ReadAll(fileReader)
+
+		rootFS.WriteFile(unzippath, data, 0755)
+	}
+
+	// 列出解压后的目录树
+	err = fs.WalkDir(rootFS, ".", func(path string, d fs.DirEntry, err error) error {
+		fmt.Println(path)
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/wsndshx/LocalizeEpub
+
+go 1.16
+
+require github.com/psanford/memfs v0.0.0-20210214183328-a001468d78ef
